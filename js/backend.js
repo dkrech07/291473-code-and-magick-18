0a08f024746e1e8aee121e0f923b582a35bd4719
Дмитрий Кречетников <dkrech07@gmail.com>
2019-10-05 21:58:19 +0500

    Вносит исправления

--- js/backend.js
+++ js/backend.js
@@ -1,12 +1,11 @@
 'use strict';
 
 (function () {
-
   var TIMEOUT_DURATION = 1000;
   var STATUS_OK = 200;
 
   window.backend = {
-    load: function (onSuccess, onError) {
+    load: function (onLoad, onError) {
       var URL = 'https://js.dump.academy/code-and-magick/data';
 
       var xhr = new XMLHttpRequest();
@@ -14,7 +13,7 @@
 
       xhr.addEventListener('load', function () {
         if (xhr.status === STATUS_OK) {
-          onSuccess(xhr.response);
+          onLoad(xhr.response);
         } else {
           onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
         }
@@ -31,16 +30,15 @@
       xhr.open('GET', URL);
       xhr.send();
     },
-    upload: function (data, onSuccess, onError) {
+    upload: function (data, onLoad, onError) {
       var URL = 'https://js.dump.academy/code-and-magick';
 
       var xhr = new XMLHttpRequest();
       xhr.responseType = 'json';
 
       xhr.addEventListener('load', function () {
-        console.log(xhr.status + ' ' + xhr.statusText);
         if (xhr.status === STATUS_OK) {
-          onSuccess(xhr.response);
+          onLoad(xhr.response);
         } else {
           onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
         }
--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,6 @@
 'use strict';
 
 (function () {
-  // var WIZARD_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  // var WIZARD_SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-  // var WIZARDS_NUMBER = 4;
-
   window.setup = {
     popUp: document.querySelector('.setup'),
     COAT_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
@@ -22,20 +18,6 @@
     return Math.floor(Math.random() * number);
   };
 
-  // var getWizardsList = function () {
-  //   var wizardsArray = [];
-  //   for (var i = 0; i < WIZARDS_NUMBER; i++) {
-  //     var wizard = {
-  //       name: WIZARD_FIRST_NAMES[getRandom(WIZARD_FIRST_NAMES.length)] + ' ' + WIZARD_SECOND_NAMES[getRandom(WIZARD_SECOND_NAMES.length)],
-  //       coatColor: window.setup.COAT_COLORS[getRandom(window.setup.COAT_COLORS.length)],
-  //       eyesColor: window.setup.EYES_COLORS[getRandom(window.setup.EYES_COLORS.length)]
-  //     };
-  //     wizardsArray.push(wizard);
-  //   }
-  //
-  //   return wizardsArray;
-  // };
-  //
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
@@ -46,21 +28,7 @@
     return wizardElement;
   };
 
-  // var addWizard = function (wizards) {
-  //   var fragment = document.createDocumentFragment();
-  //
-  //   for (var i = 0; i < WIZARDS_NUMBER; i++) {
-  //     fragment.appendChild(renderWizard(wizards[i]));
-  //   }
-  //   similarListElement.appendChild(fragment);
-  // };
-
-  // addWizard(getWizardsList());
-
-  // window.setup.popUp.querySelector('.setup-similar').classList.remove('hidden');
-
-// Загрузка данных с сервера;
-  var successHandler = function (wizards) {
+  var successLoadHandler = function (wizards) {
     var fragment = document.createDocumentFragment();
 
     for (var i = 0; i < NUMBER_CHARACTERS; i++) {
@@ -69,6 +37,10 @@
     similarListElement.appendChild(fragment);
 
     window.setup.popUp.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  var successUploadHandler = function () {
+    window.setup.popUp.classList.add('hidden');
   };
 
   var errorHandler = function (errorMessage) {
@@ -83,26 +55,14 @@
     document.body.insertAdjacentElement('afterbegin', node);
   };
 
-  window.backend.load(successHandler, errorHandler);
-
-  // window.backend.load(function (wizards) {
-  //   var fragment = document.createDocumentFragment();
-  //
-  //   for (var i = 0; i < 4; i++) {
-  //     fragment.appendChild(renderWizard(wizards[i]));
-  //   }
-  //   similarListElement.appendChild(fragment);
-  //
-  //   window.setup.popUp.querySelector('.setup-similar').classList.remove('hidden');
-  // });
+  // Загрузка данных с сервера;
+  window.backend.load(successLoadHandler, errorHandler);
 
   // Отправка данных на сервер;
   var form = window.setup.popUp.querySelector('.setup-wizard-form');
   form.addEventListener('submit', function (evt) {
     evt.preventDefault();
-    window.backend.upload(new FormData(form), function () {
-      window.setup.popUp.classList.add('hidden');
-    }, errorHandler);
+    window.backend.upload(new FormData(form), successUploadHandler, errorHandler);
   });
 
 })();
