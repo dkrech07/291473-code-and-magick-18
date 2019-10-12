bbfeb757a528c081b4fb7abaaf4ced7d1f47ef65
Дмитрий Кречетников <dkrech07@gmail.com>
2019-10-12 21:31:58 +0500

    Вносит исправление в порядок отображения цветов

--- js/wizard.js
+++ js/wizard.js
@@ -2,8 +2,6 @@
 
 (function () {
   var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-
-  // var COAT_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
